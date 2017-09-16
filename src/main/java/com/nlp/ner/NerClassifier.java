50f909f2ea608cfe94f9b83e1b1ac548bd046ed0
“Vikas <naiyarv@gmail.com>
2017-09-16 21:51:31 +0530

    Classifier is working a bit better now

---
+++ src/main/java/com/nlp/ner/NerClassifier.java
@@ -0,0 +1,48 @@
+package com.nlp.ner;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by vikasnaiyar on 16/09/17.
+ */
+public class NerClassifier {
+
+    public  static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text in the text variable
+        String text = "Java Hibernate Spring";
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for(CoreMap sentence: sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the POS tag of the token
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                // this is the NER label of the token
+                String ner = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+
+                System.out.println(word + "\t" + pos+ "\t" + ner);
+            }
+        }
+    }
+}
--- src/main/java/com/resume/classifier/CustomClassifier.java
+++ src/main/java/com/resume/classifier/CustomClassifier.java
@@ -1,16 +1,21 @@
 package com.resume.classifier;
 
 import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.io.EncodingPrintWriter;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.util.SystemUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 /**
  * Created by vikasnaiyar on 16/09/17.
  */
 public class CustomClassifier {
-
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/train/customTags.prop");
         FileInputStream fileInput = new FileInputStream(file);
@@ -21,6 +26,31 @@ public class CustomClassifier {
         final CRFClassifier crfClassifier = new CRFClassifier(properties);
         crfClassifier.train("src/main/resources/train/customTags.train");
 
-        crfClassifier.classifyAndWriteAnswers("src/main/resources/train/resume.txt");
+        List<List<CoreLabel>> coreLabels = crfClassifier.classifyFile("src/main/resources/train/resume.txt");
+
+        coreLabels.forEach(labels -> {
+            labels.forEach(label -> {
+                String answer = label.get(CoreAnnotations.AnswerAnnotation.class);
+                System.out.println(label.word() + "\t" + answer);
+            });
+        });
+
     }
 }
+
+/*
+noMidNGrams=true
+        useDisjuntive=true
+        maxNGramLeng=6
+        usePrev=true
+        useNext=true
+        useSequence=true
+        usePrevSequence=true
+        maxLeft=1
+        useTypeSeqs=true
+        useTypeSeqs2=true
+        useTypeySequence=true
+        wordShape=chris2useLC
+
+        serializeTo=custom-model.ser.qz
+*/
