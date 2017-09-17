261afd47a1d13f426ea32c822aad89e9f428355c
log10 <lokes.noble@gmail.com>
2017-09-17 22:33:45 +0530

    updating ResumeSectionReader

--- src/main/java/com/resume/reader/ResumeReader.java
+++ src/main/java/com/resume/reader/ResumeReader.java
@@ -20,18 +20,15 @@ public class ResumeReader {
      */
     public static void main(String[] args) {
         Nlp.init();
-        Sentence sentence = new SentenceParser()//.apply("A minimum of two years commercial software development experience in Java, and four years overall software development experience");
-        		.apply("Lokesh\nlokesh.noble@gmail.com\nCollege of Engineering Guindy");
-//                .apply("Lokesh is good at Object Oriented Programming concepts, Lokesh knows Typescript and Java. Lokesh graduated from College Of Engineering Guindy");
-//        			.apply("Developed dynamic and interactive website that ensured high traffic, page views, and User Experience, resulting in 40% increase in sales revenue");//\n"+
-//        		.apply("After graduating from Madras Institute of Technology (MIT – Chennai) in 1960, Dr. Abdul Kalam joined Aeronautical Development Establishment of Defence Research and Development Organisation (DRDO) as a scientist.");
-//        		.apply("Lokesh\nlokesh.noble@gmail.com\nCollege of Engineering Guindy");
-        System.out.println(sentence.getPosTaggedList());
-        System.out.println(sentence.getPersonSet());
-        System.out.println(sentence.getOrgSet());
-        System.out.println(sentence.getDuractionSet());
-        System.out.println(sentence.getSemanticGraph());
-        System.out.println(sentence.getInterestingWords());
+        new ResumeSectionReader().apply("/resume/John_Doe_cv.docx").forEach(section -> {
+	        Sentence sentence = new SentenceParser().apply(section.getValue());
+//	        System.out.println(sentence.getPosTaggedList());
+	        System.out.println("Person List: "+sentence.getPersonSet());
+	        System.out.println("Organization List: "+sentence.getOrgSet());
+	        System.out.println("Duration List: "+sentence.getDuractionSet());
+//	        System.out.println(sentence.getSemanticGraph());
+//	        System.out.println(sentence.getInterestingWords());
+        });
     }
     
 }
